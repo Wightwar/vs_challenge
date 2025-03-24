dffb46350976436be2573ec25e60d9850be8c841
Tim <tim-gerling@gmx.de>
2025-03-24 23:27:47 +0100

    Include docstring

--- src/functions.py
+++ src/functions.py
@@ -2,24 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def create_sequences(data, target_column, sequence_length=5):
+    """This function creates a sequence of lengt n from given data.
+
+    Args:
+        data (ndarray): The raw data where the sequence should be created from.
+        target_column (int): Index of the target column.
+        sequence_length (int): Length of the sequence that should be created. Defaults to 5.
+
+    Returns:
+        ndarray: An array containing the input data called x and an array containing the target data y.
+    """
     x, y = [], []
     for i in range(len(data) - sequence_length):
-        x.append(data[i:i+sequence_length])
-        y.append(data[i+sequence_length, target_column])
+        x.append(data[i:i+sequence_length]) # Last 5 hours as input
+        y.append(data[i+sequence_length, target_column]) # Next hour as target
     
     return np.array(x), np.array(y)
 
 def calc_corr_of_feature(df, features, target):
+    """This function calculates the linear correlation between a list of features and the target.
+
+    Args:
+        df (DataFrame): A data frame containing the values.
+        features (list[string]): A list of strings with features.
+        target (list[string]): A list of strings with the target.
+    """
     target_shifted = df[target].shift(-1) # Shift target to have next-hour correlation
     for feature in features:
         correlations = df[feature].corr(target_shifted)
         print(feature, "corr:", correlations)
 
 def plot_training_loss(training_loss, validation_loss):
+    """This function plots the training and the validation loss over the epochs.
+
+    Args:
+        training_loss (ndarray): An array with the training losses.
+        validation_loss (ndarray): An array with the validation losses.
+    """
     epochs = range(1, len(training_loss) + 1)
 
     plt.plot(epochs, training_loss, 'bo', label='Training loss')
     plt.plot(epochs, validation_loss, 'b', label='Validation loss')
+    plt.xlabel('epochs')
     plt.title('Training and validation loss')
     plt.legend()
 
@@ -28,6 +52,13 @@ def plot_training_loss(training_loss, validation_loss):
     plt.show()
 
 def plot_power_time(y_test, y_pred=np.array([]), y_baseline_pred=np.array([])):
+    """This function plots the power consumption and the predicted power consumption over the time.
+
+    Args:
+        y_test (ndarray): An array with the real power consumption values.
+        y_pred (ndarray): An array with the predicted power consumption values. Defaults to np.array([]).
+        y_baseline_pred (ndarray): An array with the predicted power consumption values by the baseline. Defaults to np.array([]).
+    """
     plt.figure(figsize=(10,5))
     plt.plot(y_test, label="Actual Power Consumption", color="blue")
     if y_pred.size != 0:
