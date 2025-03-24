b47a5b673dc3c646d0fb714ba55ed791ef7c48ec
Tim <tim-gerling@gmx.de>
2025-03-24 12:17:12 +0100

    Restructured notebook

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def create_sequences(data, target_column, sequence_length=5):
     x, y = [], []
@@ -7,3 +8,32 @@ def create_sequences(data, target_column, sequence_length=5):
         y.append(data[i+sequence_length, target_column])
     
     return np.array(x), np.array(y)
+
+def calc_corr_of_feature(df, features, target):
+    for feature in features:
+        event_correlations = df[target].corr(df[feature])
+        print(feature, "corr:", event_correlations)
+
+def plot_training_loss(training_loss, validation_loss):
+    epochs = range(1, len(training_loss) + 1)
+
+    plt.plot(epochs, training_loss, 'bo', label='Training loss')
+    plt.plot(epochs, validation_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    #plt.plot(epochs, mean_absolute_error, '--', label='Mean absolute error')
+
+    plt.show()
+
+def plot_power_time(y_test, y_pred=np.array([]), y_baseline_pred=np.array([])):
+    plt.figure(figsize=(10,5))
+    plt.plot(y_test, label="Actual Power Consumption", color="blue")
+    if y_pred.size != 0:
+        plt.plot(y_pred, label="Predicted Power Consumption", color="red", linestyle="dashed")
+    if y_baseline_pred.size != 0:
+        plt.plot(y_baseline_pred, label="Baseline Predicted Power Consumption", color="green", linestyle="dashed")
+    plt.xlabel("Time")
+    plt.ylabel("Power Consumption")
+    plt.legend()
+    plt.show()
